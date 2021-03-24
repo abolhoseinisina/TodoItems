2ecbafbac185c4706da779c3ddff3a10e0dfb861
ebola <abolhoseini.sina@gmail.com>
2021-03-25 03:23:27 +0430

    adding common project

---
+++ TodoItemsProject.Common/Interfaces/IDateTimeService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoItemsProject.Common.Interfaces
+{
+    public interface IDateTimeService
+    {
+        public DateTime Now { get; }
+    }
+}
---
+++ TodoItemsProject.Common/Services/DateTimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoItemsProject.Common.Interfaces;
+
+namespace TodoItemsProject.Common.Services
+{
+    class DateTimeService : IDateTimeService
+    {
+        public DateTime Now { get => DateTime.Now; }
+    }
+}
