95fefaa95dcd1ba3e13bef051f3e31c400b2f7b6
ebola <abolhoseini.sina@gmail.com>
2021-03-25 03:23:58 +0430

    Renaming the ui project to WebAPI

--- TodoItemsProject/Startup.cs
+++ TodoItemsProject/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TodoItemsProject
+namespace TodoItemsProject.WebAPI
 {
     public class Startup
     {
