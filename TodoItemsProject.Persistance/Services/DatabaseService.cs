d49dda7ce0c721b27fed112774e93328ee4e3093
ebola <abolhoseini.sina@gmail.com>
2021-03-25 01:59:58 +0430

    Creating persistance project

---
+++ TodoItemsProject.Persistance/Services/DatabaseService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoItemsProject.Application.Interfaces;
+using TodoItemsProject.Domain.Models;
+
+namespace TodoItemsProject.Persistance.Services
+{
+    public class DatabaseService : DbContext, IDatabaseService
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        public DbSet<Place> Places { get; set; }
+    }
+}
---
+++ TodoItemsProject.Persistance/Services/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoItemsProject.Domain.Models;
+
+namespace TodoItemsProject.Persistance.Services
+{
+    public class UnitOfWork
+    {
+        // Part 1
+        private DatabaseService context = new DatabaseService();
+        private GenericRepository<Todo> todoRepository;
+        private GenericRepository<Place> placeRepository;
+
+        public GenericRepository<Todo> TodoRepository
+        {
+            get
+            {
+                if (this.todoRepository == null)
+                {
+                    this.todoRepository = new GenericRepository<Todo>(context);
+                }
+                return todoRepository;
+            }
+        }
+
+        public GenericRepository<Place> PlaceRepository
+        {
+            get
+            {
+                if (this.placeRepository == null)
+                {
+                    this.placeRepository = new GenericRepository<Place>(context);
+                }
+                return placeRepository;
+            }
+        }
+
+        // Part 2
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+    }
+}
