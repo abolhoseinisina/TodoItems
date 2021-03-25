e89a69f45c43650be42a42933fba3b118185cd43
ebola <abolhoseini.sina@gmail.com>
2021-03-25 15:54:55 +0430

    making Datetime service public

--- TodoItemsProject.Common/Services/DateTimeService.cs
+++ TodoItemsProject.Common/Services/DateTimeService.cs
@@ -5,7 +5,7 @@ using TodoItemsProject.Common.Interfaces;
 
 namespace TodoItemsProject.Common.Services
 {
-    class DateTimeService : IDateTimeService
+    public class DateTimeService : IDateTimeService
     {
         public DateTime Now { get => DateTime.Now; }
     }
