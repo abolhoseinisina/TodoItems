2c5607166d636c2f31b15d5b4247397d0b19852b
ebola <abolhoseini.sina@gmail.com>
2021-03-25 16:08:32 +0430

    Creating TodoController and creating its methods

---
+++ TodoItemsProject.WebAPI/Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using TodoItemsProject.Application.Interfaces;
+using TodoItemsProject.Domain.Models;
+
+namespace TodoItemsProject.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        readonly ITodoService todoService;
+        public TodoController(ITodoService todoService)
+        {
+            this.todoService = todoService;
+        }
+        [HttpGet]
+        public IEnumerable<Todo> Get()
+        {
+            return todoService.Get();
+        }
+        [HttpGet("{id}")]
+        public Todo Get(int id)
+        {
+            return todoService.GetById(id);
+        }
+        [HttpPut]
+        public void Put(Todo item)
+        {
+            todoService.Edit(item);
+        }
+        [HttpPost]
+        public void Post(Todo item)
+        {
+            todoService.Add(item);
+        }
+        [HttpDelete]
+        public void Delete(Todo item)
+        {
+            todoService.Delete(item);
+        }
+    }
+}
